af08c89a222e2c4d9141a303d1a78063204d96c5
bhargav-13 <thesiyabhargav11@gmail.com>
2022-12-23 12:13:53 +0530

    commit

--- random/ADTarray.cpp
+++ random/ADTarray.cpp
@@ -1,18 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+struct myarray{
     int *a;
     int size;
     int length;
+};
+
+void createarray(struct myarray *a,int tsize,int usize){
+    (*a).size=tsize;
+}
+
+int main(int argc, char const *argv[])
+{
+    struct myarray marks;
+    {
+        /* data */
+    };
+    
 
     cout << "enter size:";
     cin >> size;
 
     a=new int[size];
     
-
+    createarray(&marks,100,20);
 
     return 0;
 }
