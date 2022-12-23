78f4d7555d40d927f4239b9059bb4ab9269880dd
bhargav-13 <thesiyabhargav11@gmail.com>
2022-12-23 10:18:39 +0530

    second commit

---
+++ random/ADTarray.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int *a;
+    int size;
+    int length;
+
+    cout << "enter size:";
+    cin >> size;
+
+    a=new int[size];
+    
+
+
+    return 0;
+}
