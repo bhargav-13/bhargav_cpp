872a37304ccc952b025b9f1657a3aafac67ea49e
bhargav-13 <thesiyabhargav11@gmail.com>
2023-01-26 17:19:15 +0530

    commits

---
+++ codechef practice/apples.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+   int t;
+   cin >> t;
+
+   while(t--){
+       int n,m;
+       
+   }
+   return 0;
+}
