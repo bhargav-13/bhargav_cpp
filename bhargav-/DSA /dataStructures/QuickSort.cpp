a4aac789f50f38041416c1282424b211467d2bfd
bhargav-13 <thesiyabhargav11@gmail.com>
2023-01-30 20:44:47 +0530

    commits

---
+++ bhargav-/DSA /dataStructures/QuickSort.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print (int arr[], int n)
+{
+    
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << flush;
+        this_thread::sleep_for(chrono::milliseconds(200));
+        if(i<n-1){
+            cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
+int partition(int arr[], int s, int e){
+    int pivot = arr[s];
+    int count = 0;
+
+    for(int i=s+1; i<=e; i++){
+        if(arr[i] <= pivot){
+            count++;
+        }
+    }
+    int pivotindex = s + count;
+    swap(arr[s], arr[pivotindex]);
+
+    int i=s, j=e;
+    while(i < pivotindex && j > pivotindex){
+        while(arr[i] < pivot){
+        i++;
+        }
+
+        while(arr[j] > pivot){
+        j--;
+        }
+
+        if(i < pivotindex && j > pivotindex){
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotindex; 
+}
+
+void Quicksort(int arr[], int s, int e){
+       if(s >= e){
+        return ;
+       }
+       int p = partition(arr,s, e);
+       Quicksort(arr,s, p-1);
+       Quicksort(arr,p+1, e);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {5,8,15,2,6,89};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "array is: ";
+    print(arr,size);
+    cout << endl;
+    Quicksort(arr,0,size-1);
+    cout << "Soted array is: ";
+    print(arr,size);
+    return 0;
+}
--- bhargav-/DSA /dataStructures/SelectionSort.cpp
+++ bhargav-/DSA /dataStructures/SelectionSort.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void Selsort(int arr[], int n){
-    // cout << arr[0] << endl;
-    // cout << n << endl;
+void print (int arr[], int n)
+{
     
-    // cout << minelm << endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << flush;
+        this_thread::sleep_for(chrono::milliseconds(200));
+        if(i<n-1){
+            cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
+void Selsort(int arr[], int n){
     for(int i=0; i<n-1; i++)
     {
         int minindex = i;
@@ -27,10 +37,11 @@ int main(int argc, char const *argv[])
 {
     int arr[] = {5,8,15,2,6,89};
     int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "array is: ";
+    print(arr,size);
+    cout << endl;
     Selsort(arr,size);
-    for(int i=0; i<size; i++)
-    {
-        cout << arr[i] << ", ";
-    }
+    cout << "Soted array is: ";
+    print(arr,size);
     return 0;
 }
