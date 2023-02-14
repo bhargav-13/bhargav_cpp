980a003a6c6ca89cf842fdd40f344c4f692a9a52
bhargav-13 <thesiyabhargav11@gmail.com>
2023-02-15 01:33:00 +0530

    commits

--- bhargav-/DSA /dataStructures/LinkedList.cpp
+++ bhargav-/DSA /dataStructures/LinkedList.cpp
@@ -20,9 +20,32 @@ void display(node* &head)
    }
    node* temp = head;
    while(temp != NULL){
-    cout << temp -> data << endl;
+    cout << temp -> data << " ";
     temp = temp -> next;
    }
+   cout << endl;
+}
+
+void insertbyTail(node* &tail, int data){
+    node *temp = new node(data);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void insert(node* &head, int pos, int data){
+    node* temp = new node(data);
+    if(pos == 1){
+        temp -> next = head;
+        head = temp;
+    }
+    else{
+    for(int i=0; i<pos-1; i++){
+        head = head -> next;
+        temp -> next = head -> next;
+        head -> next = temp; 
+        }
+    }
+
 }
 
 
@@ -31,6 +54,10 @@ int main(int argc, char const *argv[])
 {
         node* n1 = new node(10);
         node* n2 = new node(20);
+        node* n3 = new node(30);
         node* head  = n1;
+        n1 -> next = n2;
+        n2 -> next = n3;
+        insert(head, 3, 66);
         display(head);
 }
