fd3edfaf6b4af9ce13ac2087a51cfc84804132c9
bhargav-13 <thesiyabhargav11@gmail.com>
2023-02-24 22:08:14 +0530

    commits

--- bhargav-/DSA /dataStructures/CircularLL.cpp
+++ bhargav-/DSA /dataStructures/CircularLL.cpp
@@ -10,6 +10,16 @@ class node{
         this -> data = data;
         this ->next = NULL;
     }
+
+    ~node()
+    {
+        int value = this->data;
+        if(this->next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
 };
 
 void display(node* &tail)
@@ -48,6 +58,38 @@ void insertatPosition(node* &tail, int pos, int data){
     }
     }
 
+void deleteNode(node* &tail, int value) {
+
+    if(tail == NULL) {
+        cout << " List is empty, please check again" << endl;
+        return;
+    }
+    else{
+
+        node* prev = tail;
+        node* curr = prev -> next;
+
+        while(curr -> data != value) {
+            prev = curr;
+            curr = curr -> next;
+        }
+
+        prev -> next = curr -> next;
+
+        if(curr == prev) {
+            tail = NULL;
+        }
+
+        else if(tail == curr ) {
+            tail = prev;
+        }
+
+        curr -> next = NULL;
+        delete curr;
+
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -57,6 +99,10 @@ int main(int argc, char const *argv[])
     insertatPosition(tail, 3, 6);
     insertatPosition(tail, 3, 4);
     insertatPosition(tail, 4, 5);
+
+    display(tail);
+     
+    deleteNode(tail, 3);
 
     display(tail);
 
