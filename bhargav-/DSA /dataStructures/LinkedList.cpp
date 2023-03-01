47837179988d053841d4ea6a8445d8c08a37245b
bhargav-13 <thesiyabhargav11@gmail.com>
2023-03-02 00:51:48 +0530

    2march

--- bhargav-/DSA /dataStructures/LinkedList.cpp
+++ bhargav-/DSA /dataStructures/LinkedList.cpp
@@ -96,22 +96,56 @@ p=p->next;
 return 1;
 }
 
-void removeDuplicats(node* &head){
-      node* p = head;
-      node* q = head->next;
 
-      while(p->next != NULL){
-        if(p->data == q->data){
-            p -> next = q -> next;
-            delete q;
-            q = p->next;
+void removeDuplicats(node* &head){
+ 	//empty List
+    if(head == NULL)
+        return;
+    
+    //non empty list
+    node* curr = head;
+    
+    while(curr != NULL) {
+        
+        if( (curr -> next != NULL) && curr -> data == curr -> next -> data) {
+            node* next_next = curr ->next -> next;
+            node* nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
         }
-        else{
-            p = q;  
-            q = q->next;
-        }
-      }
+        else //not equal
+        {
+            curr = curr -> next;
+        }   
+    }
 }
+
+void removeDuplicatsFromUS(node* &head){
+    node *ptr1, *ptr2, *dup;
+    ptr1 = head;
+ 
+    /* Pick elements one by one */
+    while (ptr1 != NULL && ptr1->next != NULL) {
+        ptr2 = ptr1;
+ 
+        /* Compare the picked element with rest
+           of the elements */
+        while (ptr2->next != NULL) {
+            /* If duplicate then delete it */
+            if (ptr1->data == ptr2->next->data) {
+                /* sequence of steps is important here */
+                dup = ptr2->next;
+                ptr2->next = ptr2->next->next;
+                delete dup;
+            }
+            else /* This is tricky */
+                ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+        
+}
+
 
 void reverseLL(node* &head){
     node* p = head;
@@ -139,17 +173,23 @@ int main(int argc, char const *argv[])
         insertbyTail(tail, 30);
         insertbyTail(tail, 40);
         insertbyTail(tail, 50);
+        insertbyTail(tail, 40);
         insertbyTail(tail, 60);
+        display(head);
+
+        
 
 //        insert(head, tail, 5, 45);
         
 //        deletebyIndex(head, 4);
         
-//        removeDuplicats(head);
+    //works only for sorted LL
+    // removeDuplicats(head);
+    removeDuplicatsFromUS(head);
         
-       reverseLL(head);
+//        reverseLL(head);
 
        
-        //isSorted(head)?(cout << "sorted" << endl) : (cout << "not sorted" << endl);
+        isSorted(head)?(cout << "sorted" << endl) : (cout << "not sorted" << endl);
         display(head);
 }
