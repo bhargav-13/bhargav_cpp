093f0a297bbe00426a6a84c2531273745aa1f10b
bhargav-13 <bhargavthesiya11@gmail.com>
2023-03-14 01:50:57 +0530

    commits

---
+++ bhargav-/DSA /dataStructures/Stack.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    public:
+    int *arr;
+    int top;
+    int size;
+
+    Stack(int size){
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+    void push(int elm){
+        if(top < size-1){
+        top++;
+        arr[top] = elm;
+        }
+        else{
+            cout << "Stack Overflow" << endl;
+        }
+    }
+
+    void pop(){
+        if(top >= 0){
+            top--;
+        }
+        else{
+            cout << "Stack Underflow" << endl;
+        }
+    }
+
+    int peek() {
+        if(top >=0 )
+            return arr[top];
+        else
+        {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+    }
+
+    void print(){
+        while(top != -1){
+            cout << arr[top] << " "<< flush;
+            this_thread::sleep_for(chrono::milliseconds(200));
+            top--;
+        }
+    }
+
+
+};
+
+int main(int argc, char const *argv[])
+{
+    Stack st(5);
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(3);
+    st.push(3);
+
+    st.print();    
+    return 0;
+}
